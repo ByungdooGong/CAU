1946e80c8432e179d360ac38f2cca60f01e7757e
byungdoo <gbd4789@naver.com>
2019-09-13 19:42:22 +0900

    trash

--- HandWittenCalculator-master-20190802T112737Z-001/HandWittenCalculator-master/discrimination.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-def discriminate(sess, input):
-    labelResult = sess.run(tf.argmax(logits, 1), feed_dict={X: input, keep_prob: 1})
-    testResult = [answer[i] for i in labelResult]
--- HandWittenCalculator-master-20190802T112737Z-001/HandWittenCalculator-master/normalization.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def normalize(dataset):
-    result = np.subtract(dataset, np.mean(dataset))
-    result = np.divide(result, np.std(dataset))
-    return result
